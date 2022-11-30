803dfed177276e93e82111a53449ead37be31dcf
UsernameTaken4 <74597434+UsernameTaken4@users.noreply.github.com>
2022-11-30 11:56:45 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <Header />
+    <hr />
+    <Main />
+    <hr />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Main from './components/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    Main,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+hr {
+  margin-top: 30px;
+  margin-bottom: 40px;
+}
+body {
+  /* background-color: paperwhite; */
+  opacity: 0.9;
+}
+
+a {
+}
+</style>
